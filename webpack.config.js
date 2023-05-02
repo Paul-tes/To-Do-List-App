646b770f2622fa85f811e8596240c3704caf07ad
paul-tes <paulasca8462@gmail.com>
2023-05-02 23:54:45 +0300

    added html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do App',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
