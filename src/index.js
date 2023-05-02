e2551b1bcdb5c0eaa49c8abb8642946c10b885db
paul-tes <paulasca8462@gmail.com>
2023-05-03 02:05:11 +0300

    task list designed in html file

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './styles/style.css';
+
+const todoLists = [
+  {
+    index: 1,
+    desctiption: 'Play Guitar',
+    completed: true,
+  },
+  {
+    index: 2,
+    desctiption: 'Read ES6 syntax',
+    completed: false,
+  },
+  {
+    index: 3,
+    desctiption: 'Get Ready for WebPack',
+    completed: true,
+  },
+]
+
+const displayLists = () => {
+  
+}
