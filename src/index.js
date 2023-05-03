4627df97f4d52951b77681be5fa0b055f2376d30
paul-tes <paulasca8462@gmail.com>
2023-05-03 11:45:18 +0300

    added dynamic task lists

--- src/index.js
+++ src/index.js
@@ -19,5 +19,23 @@ const todoLists = [
 ]
 
 const displayLists = () => {
-  
+  const lists = todoLists;
+  const taskContainer = document.querySelector('.tasks-container');
+  console.log(taskContainer);
+  lists.forEach(t => {
+    const task = document.createElement('div');
+    task.innerHTML = `
+    <div class="task-cont">
+      <input type="checkbox" name="task" id="task-check-box">
+      <input type="text" value="${t.desctiption}" class="task-value">
+    </div>
+    <div class="opp-icon">
+      <i class="fa fa-ellipsis-v dots-icon"></i>
+    </div>
+    `
+    task.classList.add('task');
+    taskContainer.appendChild(task);
+  });
 }
+
+displayLists();
