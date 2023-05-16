9c6a2997c532c0ed88e03b199a9fabbd9e1b0b73
paul-tes <paulasca8462@gmail.com>
2023-05-16 09:33:45 +0300

    updatetestcase and clearCompleteTasks test are introduce

--- __Test__/Store.js
+++ __Test__/Store.js
@@ -22,8 +22,31 @@ export default class Store {
       i += 1;
     });
     LocalStorage.setItem(tasks);
+    LocalStorage.LASTINDEX = tasks.length;
   }
 
+  updateTask(index, description) {
+    const tasks = LocalStorage.getItems();
+    tasks.forEach((task) => {
+      if (task.index === Number(index)) {
+        task.description = description;
+      }
+    });
+    LocalStorage.setItem(tasks);
+    LocalStorage.LASTINDEX = tasks.length;
+  }
+
+  clearCompleteTasks() {
+    let tasks = LocalStorage.getItems();
+    tasks = tasks.filter((task) => task.completed === false);
+    let i = 1;
+    tasks.forEach((task) => {
+      task.index = i;
+      i += 1;
+    });
+    LocalStorage.setItem(tasks)
+    LocalStorage.LASTINDEX = tasks.length;
+  }
 
 }
 
--- __Test__/Store.test.js
+++ __Test__/Store.test.js
@@ -1,11 +1,12 @@
 import LocalStorage from "../__Mock__/LocalStorage.js";
 import Store from "./Store.js";
 import Task from "../src/modules/Task.js";
+import { experiments } from "webpack";
 
-describe("Store operational functions Test", ()=> {
+describe("Store operational functions Test", () => {
 
   // addTask Test Case [1]
-  test('when The new task is added it should be shown on the store', ()=> {
+  test('when The new task is added it should be shown on the store', () => {
     // Arrange
     const store = new Store();
     const task = new Task(LocalStorage.items.length + 1, 'Hello Task 1', false);
@@ -19,7 +20,7 @@ describe("Store operational functions Test", ()=> {
   })
 
   // addtask Test Case [2]
-  test('when The new task is added it should be shown on the store', ()=> {
+  test('when The new task is added it should be shown on the store', () => {
     // Arrange
     const store = new Store();
     const task = new Task(LocalStorage.getItems().length + 1, 'Hello Task 2', false);
@@ -33,7 +34,7 @@ describe("Store operational functions Test", ()=> {
   })
 
   // removeTask test case [1]
-  test('when the task is removed from Store it should be not inside LocalStorage', ()=> {
+  test('when the task is removed from Store it should be not inside LocalStorage', () => {
     // Arrange
     const store = new Store();
     const length = LocalStorage.getItems().length;
@@ -46,7 +47,7 @@ describe("Store operational functions Test", ()=> {
   })
 
   // removeTask test case [2]
-  test('when the task is removed from Store it should be not inside LocalStorage', ()=> {
+  test('when the task is removed from Store it should be not inside LocalStorage', () => {
     // Arrange
     const store = new Store();
     const length = LocalStorage.getItems().length;
@@ -57,4 +58,37 @@ describe("Store operational functions Test", ()=> {
     // assert
     expect(localStorageLenght).toBeLessThan(length);
   })
+
+  // updateTask Test case[1]
+  test('When the the update method is called with a prameter description the task should be updated to the parameter passed description', () => {
+    // Arrange
+    const store = new Store();
+    const task = new Task(LocalStorage.getItems().length + 1, 'Task 2', false);
+    store.addTask(task);
+
+    // Act
+    store.updateTask(1, 'Task 1');
+    const updatedTask = LocalStorage.getItems()[0];
+
+    // assert
+    expect(updatedTask.description).toBe('Task 1');
+  })
+
+  // clearCompleteTasks test case [1]
+  test('When the clearCompleteTasks method is called all completed tasks should be delleted from the local storage', () => {
+    // Arrange
+    const store = new Store();
+    let task1 = new Task(LocalStorage.getItems().length + 1, 'Task 1', false);
+    store.addTask(task1);
+    task1 = new Task(LocalStorage.getItems().length + 1, 'Task 2', true);
+    store.addTask(task1);
+
+    // Act
+    store.clearCompleteTasks();
+    const tasks = LocalStorage.getItems().filter(task => task.completed)
+    let none = [];
+    // assert
+    expect(tasks).toEqual(none);
+  })
+  // Arrange
 })
